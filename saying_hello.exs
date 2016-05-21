ad66d5bbf58e1c85e6e786ba19fb579e1d6b708f
Brian Davis <mrbriandavis@outlook.com>
2016-05-21 10:56:44 -0700

    hello_world excercise with tests

---
+++ saying_hello.exs
@@ -0,0 +1,40 @@
+defmodule HelloWord do
+
+  # docstring are quite useful you can generate docs out of them
+  # check: http://elixir-lang.org/getting-started/module-attributes.html
+  # for more info
+  @doc """
+  Gets name from standard IO
+  """
+  def request_name do
+    IO.puts "What is your name"
+  end
+
+  def get_name do
+    IO.gets(request_name)
+    |> String.strip
+    |> String.capitalize
+  end
+end
+
+ExUnit.start
+
+defmodule HelloWorldTest do
+
+  use ExUnit.Case, async: true
+
+  import ExUnit.CaptureIO
+
+  test "requests to input name" do
+    assert capture_io(fn ->
+      HelloWord.request_name
+    end) == "What is your name\n"
+  end
+
+  test "capitalizes input on name" do
+    capture_io([input: "sam\n", capture_prompt: false], fn ->
+      name = HelloWord.get_name
+      assert name == "Sam"
+    end)
+  end
+end
