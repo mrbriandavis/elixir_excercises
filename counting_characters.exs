a42c20a99a22dfa43177b706818b4fd51f35d83c
Brian Davis <mrbriandavis@outlook.com>
2016-05-22 09:26:49 -0700

    added check for empty word

--- counting_characters.exs
+++ counting_characters.exs
@@ -15,7 +15,14 @@ defmodule CountingCharacters do
   Builds output string
   """
   def output_phrase(length, string) do
-    IO.write "#{string} has #{length} characters"
+    IO.puts "#{string} has #{length} characters"
+  end
+
+  @doc """
+  Checks string if empty
+  """
+  def valid_word(word) do
+    word != ""
   end
 
   @doc """
@@ -25,18 +32,25 @@ defmodule CountingCharacters do
     IO.gets("What is the input string?")
     |> String.strip
   end
+  @doc """
+  builds empty message string
+  """
+  def empty_response_message do
+    IO.puts "Please provide a word to count."
+  end
 
   @doc """
   Builds output string
   """
   def count_word_and_output do
     my_word = get_word
-    my_word
-    |> count_characters
-    |> output_phrase(my_word)
+    if valid_word(my_word) do
+      my_word |> count_characters |> output_phrase(my_word)
+    else
+      empty_response_message
+      count_word_and_output
+    end
   end
-
-
 end
 
 ExUnit.start
@@ -54,7 +68,7 @@ defmodule CountingCharactersTest do
   test "outputs phrase" do
     assert capture_io(fn ->
       CountingCharacters.output_phrase(1, "foo")
-    end) == "foo has 1 characters"
+    end) == "foo has 1 characters\n"
   end
 
   test "outputs name with no newline" do
@@ -64,10 +78,14 @@ defmodule CountingCharactersTest do
     end)
   end
 
-  test "end to end test" do
+  test "valid input results in correct output" do
     assert capture_io([input: "foo\n", capture_prompt: false], fn ->
       CountingCharacters.count_word_and_output
-    end) == "foo has 3 characters"
+    end) == "foo has 3 characters\n"
+  end
+
+  test "invalid input results " do
+      refute CountingCharacters.valid_word("")
   end
 
 end
